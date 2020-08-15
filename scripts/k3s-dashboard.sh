aeae80a438a91d2728a228379c2eacc8de421bc3
seemywingz <kevin.jayne@icloud.com>
2020-08-14 22:53:20 -0400

    update ingress and lb

--- scripts/k3s-dashboard.sh
+++ scripts/k3s-dashboard.sh
@@ -2,16 +2,14 @@
 
 
 readonly dashboardURL=http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/#/login
-K3SCONFIG=~/.kube/k3sconfig
 
 # Get admin-user token
-kubectl --kubeconfig ${K3SCONFIG} -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
-
-# echo $k8sToken | pbcopy
+k8sToken=$(kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}') | grep "token:   " | awk '{print $2}')
+echo $k8sToken | pbcopy
 
 echo
 echo Kubernetes Dashboard URL: $dashboardURL
-# echo A valid K8S token was saved to your clipboard 
+echo A valid K8S token is saved to your clipboard 
 kubectl proxy
 
 
--- scripts/k3s-lb.sh
+++ scripts/k3s-lb.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+kubectl create ns infra
+
 # Install MetalLB
-helm update --install metallb stable/metallb --namespace kube-system \
+helm upgrade --install metallb stable/metallb --namespace infra \
   --set configInline.address-pools[0].name=default \
   --set configInline.address-pools[0].protocol=layer2 \
   --set configInline.address-pools[0].addresses[0]=10.0.1.1-10.0.1.254
 
 # Install Nginx Ingress
-helm update --install nginx-ingress stable/nginx-ingress --namespace kube-system \
+helm upgrade --install nginx-ingress stable/nginx-ingress --namespace infra \
   --set defaultBackend.enabled=false
