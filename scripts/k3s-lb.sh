9afdc78c88349687ada2c0e52e957acb2a6c91b9
seemywingz <kevin.jayne@icloud.com>
2020-08-17 19:46:35 -0400

    lb ns

--- scripts/k3s-lb.sh
+++ scripts/k3s-lb.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-kubectl create ns infra
-
 # Install MetalLB
-helm upgrade --install metallb stable/metallb --namespace infra \
+helm upgrade --install metallb stable/metallb --namespace kube-system \
   --set configInline.address-pools[0].name=default \
   --set configInline.address-pools[0].protocol=layer2 \
   --set configInline.address-pools[0].addresses[0]=10.0.1.1-10.0.1.254
 
 # Install Nginx Ingress
-helm upgrade --install nginx-ingress stable/nginx-ingress --namespace infra \
+helm upgrade --install nginx-ingress stable/nginx-ingress --namespace kube-system \
   --set defaultBackend.enabled=false
